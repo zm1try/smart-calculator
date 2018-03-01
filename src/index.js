46edaf8c8a526dea12a18c528702eca9fa63cadf
zm1try <zmitryarosh@gmail.com>
2018-03-01 17:01:35 +0300

    almost done

--- src/index.js
+++ src/index.js
@@ -1,26 +1,140 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.stack1 = [initialValue];
+    this.stack2 = [];
+    SmartCalculator.prototype.valueOf = function() {
+      return this.result();
+    }
+    SmartCalculator.prototype.toString = function() {
+      return this.result();
+    }
+    SmartCalculator.prototype.result = function() {
+      var t2 = this.stack2.length;
+      for(let i = 0; i < t2; i++) {
+        this.stack1.push(this.stack2.pop());
+        t2--;
+      }
+      // var t = this.stack1.length;
+      // for(let i = 0; i < t;) {
+      //     switch(this.stack1[i]) {
+      //       case '+': 
+      //         this.stack1[i-2] = this.stack1[i-2] + this.stack1[i-1];
+      //         this.stack1.splice(this.stack1[i-1],2);
+      //         t -= 2;
+      //         i -= 1;
+      //         break;
+      //       case '-': 
+      //         this.stack1[i-2] = this.stack1[i-2] - this.stack1[i-1];
+      //         this.stack1.splice(this.stack1[i-1],2);
+      //         t -= 2;
+      //         i -= 1;
+      //         break;
+      //       case '*': 
+      //         this.stack1[i-2] = this.stack1[i-2] * this.stack1[i-1];
+      //         this.stack1.splice(this.stack1[i-1],2);
+      //         t -= 2;
+      //         i -= 1;
+      //         break;
+      //       case '^': 
+      //         this.stack1[i-2] = Math.pow(this.stack1[i-2], this.stack1[i-1]);
+      //         this.stack1.splice(this.stack1[i-1],2);
+      //         t -= 2;
+      //         i -= 1;
+      //         break;
+      //       default:
+      //         i += 1;
+      //         break;
+      //     }
+      // }
+      console.log(this.stack1);
+    }
   }
 
+  // result() {
+  //   for(let i = 0; i < this.stack2.length; i++) {
+  //     this.stack1.push(this.stack2.pop());
+  //   }
+  //   for(let i = 0; i < this.stack1.length; i++) {
+  //     if (this.stack1[i] === '-' || this.stack1[i] === '+' || this.stack1[i] === '*' || this.stack1[i] === '/' || this.stack1[i] === '^') {
+  //       switch(this.stack1[i]) {
+  //         case '+': 
+  //           this.stack1[i-2] = this.stack1[i-2] + this.stack1[i-1];
+  //           this.stack1.splice(this.stack1[i-1],2);
+  //           i -= 2;
+  //           break;
+  //         case '-': 
+  //           this.stack1[i-2] = this.stack1[i-2] - this.stack1[i-1];
+  //           this.stack1.splice(this.stack1[i-1],2);
+  //           i -= 2;
+  //           break;
+  //         case '*': 
+  //           this.stack1[i-2] = this.stack1[i-2] * this.stack1[i-1];
+  //           this.stack1.splice(this.stack1[i-1],2);
+  //           i -= 2;
+  //           break;
+  //         case '^': 
+  //           this.stack1[i-2] = Math.pow(this.stack1[i-2], this.stack1[i-1]);
+  //           this.stack1.splice(this.stack1[i-1],2);
+  //           i -= 2;
+  //           break;
+  //         default: 
+  //           break;
+  //       }
+  //     }
+  //   }
+  //   console.log(this.stack1);
+  //   return this
+  // }
+
   add(number) {
-    // your implementation
+    
+    this.stack1.push(number);
+    this.stack2.push('+');
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    // for(let i = 0; i < stack2.length; i++) {
+    //   this.stack1.push(stack2.pop());
+    // }
+    this.stack1.push(-number);
+    this.stack2.push('+');
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.stack1.push(number);
+    this.stack1.push('*');
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.stack1.push(number);
+    this.stack1.push('/');
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    if (this.stack1[this.stack1.length-1] === '^') {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    if (this.stack1[this.stack1.length-1] === '*' || this.stack1[this.stack1.length-1] === '/') {
+      this.stack2.push(this.stack1.pop());
+      this.stack1.push(number);
+      this.stack1.push('^');
+      this.stack1.push(this.stack2.pop());
+    }
+    else {
+      this.stack1.push(number);
+      this.stack1.push('^');
+    }
+      
+    if (this.stack2[this.stack2.length-1] === '^') {
+      this.stack1.push(this.stack2.pop());
+    }
+     
+    return this;
   }
 }
 
