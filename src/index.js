2da18288a6101883d5da8eb02bf05f82da77762c
zm1try <zmitryarosh@gmail.com>
2018-03-01 22:25:17 +0300

    301/301

--- src/index.js
+++ src/index.js
@@ -5,76 +5,57 @@ class SmartCalculator {
     SmartCalculator.prototype.valueOf = function() {
       return this.result();
     }
-    // SmartCalculator.prototype.toString = function() {
-    //   return this.result();
-    // }
     SmartCalculator.prototype.result = function() {
-      console.log(this.stack1);
-      console.log(this.stack2);
-      let t2 = this.stack2.length;
-      while (t2 > 0) {
+      let l2 = this.stack2.length;
+      while (l2 > 0) {
         this.stack1.push(this.stack2.pop());
-        t2--;
+        l2--;
       }
-      let t = this.stack1.length;
-      for(let i = 0; i < t; i++) {
+      let l1 = this.stack1.length;
+      for(let i = 0; i < l1; i++) {
           switch(this.stack1[i]) {
             case '+': { 
-              //console.log(this.stack1);
               this.stack1[i-2] = this.stack1[i-2] + this.stack1[i-1];
               this.stack1.splice(i-1,2);
-              t -= 2;
+              l1 -= 2;
               i -= 2; 
-              //console.log(this.stack1);
               break; 
             }
             case '-': { 
-              //console.log(this.stack1);
               this.stack1[i-2] = this.stack1[i-2] - this.stack1[i-1];
               this.stack1.splice(i-1,2);
-              t -= 2;
+              l1 -= 2;
               i -= 2; 
-              //console.log(this.stack1);
               break; 
             }
             case '*': {
-              //console.log(this.stack1);
               if (this.stack1[i-2] < 0 && this.stack1[i-1] < 0) 
                 this.stack1[i-2] = - this.stack1[i-2] * this.stack1[i-1];
               else
                 this.stack1[i-2] = this.stack1[i-2] * this.stack1[i-1];
               this.stack1.splice(i-1,2);
-              t -= 2;
+              l1 -= 2;
               i -= 2; 
-              //console.log(this.stack1);
               break; }
             case '/': {
-              //console.log(this.stack1);
               if (this.stack1[i-2] < 0 && this.stack1[i-1] < 0) 
                 this.stack1[i-2] = - this.stack1[i-2] / this.stack1[i-1];
               else
                 this.stack1[i-2] = this.stack1[i-2] / this.stack1[i-1];
               this.stack1.splice(i-1,2);
-              t -= 2;
+              l1 -= 2;
               i -= 2;
-              //console.log(this.stack1);
               break; }
             case '^': {
-              //console.log(this.stack1);
-              // while (this.stack1[i+2] < t && this.stack1[i+2] === '^') {
-              //   i += 2;
-              // }
-               if(this.stack1[i] === '^') {
-                if (this.stack1[i-2] < 0 && this.stack1[i-1] % 2 === 0) 
-                  this.stack1[i-2] = - Math.pow(this.stack1[i-2], this.stack1[i-1]);
-                else
-                  this.stack1[i-2] = Math.pow(this.stack1[i-2], this.stack1[i-1]);
-                this.stack1.splice(i-1,2);
-                t -= 2;
-                i -= 2;
-              }
+              if (this.stack1[i-2] < 0 && this.stack1[i-1] % 2 === 0) 
+                this.stack1[i-2] = - Math.pow(this.stack1[i-2], this.stack1[i-1]);
+              else
+                this.stack1[i-2] = Math.pow(this.stack1[i-2], this.stack1[i-1]);
+              this.stack1.splice(i-1,2);
+              l1 -= 2;
+              i -= 2;
               break;
-            }
+              }
             default:
               break;
           }
@@ -86,63 +67,37 @@ class SmartCalculator {
   add(number) {
     this.stack1.push(number);
     this.stack2.push('+');
-    console.log(this.stack1);
-    console.log(this.stack2);
     return this;
   }
   
   subtract(number) {
     this.stack1.push(-number);
     this.stack2.push('+');
-    console.log(this.stack1);
-    console.log(this.stack2);
     return this;
   }
 
   multiply(number) {
     this.stack1.push(number);
     this.stack1.push('*');
-    console.log(this.stack1);
-    console.log(this.stack2);
     return this;
   }
 
   devide(number) {
     this.stack1.push(number);
     this.stack1.push('/');
-    console.log(this.stack1);
-    console.log(this.stack2);
     return this;
   }
 
   pow(number) {
-    while (this.stack1[this.stack1.length-1] == '^') {
+
+    while (this.stack1[this.stack1.length-1] == '^' || this.stack1[this.stack1.length-1] == '*' || this.stack1[this.stack1.length-1] == '/') {
       this.stack2.push(this.stack1.pop());
     }
-
-    if (this.stack1[this.stack1.length-1] == '*' || this.stack1[this.stack1.length-1] == '/') {
-      this.stack2.push(this.stack1.pop());
       this.stack1.push(number);
       this.stack1.push('^');
-      while (this.stack2[this.stack2.length-1] == '^') {
-        this.stack1.push(this.stack2.pop());
-      }
+    while (this.stack2[this.stack2.length-1] == '^' || this.stack2[this.stack2.length-1] == '*' || this.stack2[this.stack2.length-1] == '/') {
       this.stack1.push(this.stack2.pop());
     }
-    else {
-      this.stack1.push(number);
-      this.stack1.push('^');
-      while (this.stack2[this.stack2.length-1] == '^') {
-        this.stack1.push(this.stack2.pop());
-      }
-    }
-      
-    // while (this.stack2[this.stack2.length-1] === '^') {
-    //   this.stack1.push(this.stack2.pop());
-    // }
-     
-    console.log(this.stack1);
-    console.log(this.stack2);
     return this;
   }
 }
